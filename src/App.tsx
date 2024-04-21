ce2ebcd5808b41ab3d565812a9f598efe4b10b5a
KvashninAlexander2023 <study_python@mail.ru>
2024-04-21 20:21:19 +0500

    add 18 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import "./App.css";
 import { Header } from "./components/Header/Header";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Profile } from "./components/Profile/Profile";
+import { Dialogs } from "./components/Dialogs/Dialogs";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <NavBar />
-      <Profile />
+      {/* <Profile /> */}
+      <div className="app-wrapper-content">
+
+        <Dialogs />
+      </div>
     </div>
   );
 };
